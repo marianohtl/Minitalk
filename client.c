270e619c57dec8f014002605fa105625e388224a
tmariano <thalita@DESKTOP-54M218S.localdomain>
2022-11-22 13:06:43 -0300

    including update to inter processes communication

--- client.c
+++ client.c
@@ -3,8 +3,68 @@
 #include<signal.h>
 #include<stdio.h>
 
-int main(void)
+static unsigned int	ft_skip_spaces(const char *nptr)
 {
-	kill( 8166 ,SIGUSR1);
+	unsigned int	skip;
+
+	skip = 0;
+	while (nptr[skip] == ' ' || nptr[skip] == '\f'
+		|| nptr[skip] == '\n' || nptr[skip] == '\r'
+		|| nptr[skip] == '\v' || nptr[skip] == '\t')
+		skip++;
+	return (skip);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	unsigned int	skip;
+	unsigned int	digit;
+	unsigned int	mult;
+	int				result;
+
+	result = 0;
+	mult = 1;
+	skip = ft_skip_spaces(nptr);
+	if (nptr[skip] == '+' || nptr[skip] == '-')
+	{
+		if (nptr[skip] == '-')
+			mult = -1;
+		skip++;
+	}
+	digit = skip;
+	while (nptr[digit] >= '0' && nptr[digit] <= '9')
+		digit++;
+	while (digit > skip)
+	{
+		digit--;
+		result += (nptr[digit] - '0') * mult;
+		mult = mult * 10;
+	}
+	return (result);
+}
+
+int main(int argc, char **argv)
+{
+	static int index;
+	static int index_string;
+	const unsigned char	bit = 0b10000000;
+
+	if(argc != 3)
+		return 1;
+
+	while(argv[2][index_string] != '\0')
+	{
+		while (index < 8)
+		{
+			if((argv[2][index_string] & (bit >> index)) != 0)
+				kill(ft_atoi(argv[1]) , SIGUSR2);
+			else
+				kill(ft_atoi(argv[1]) , SIGUSR1);
+			index++;
+			usleep(40);
+		}
+		index = 0;
+		index_string++;
+	}
 	return(0);
 }
--- server.c
+++ server.c
@@ -3,9 +3,29 @@
 #include<signal.h>
 #include<stdio.h>
 
-void scream(int a){
-	(void) a;
-	write(STDOUT_FILENO,"AAAAAAH!\n",9);
+typedef struct s_queue
+{
+	char * content;
+	int start;
+	int end;
+} t_queue;
+
+
+static t_queue queue;
+
+void scream(int signal_number){
+	static int index;
+	const unsigned char	bit = 0b10000000;
+
+	if(signal_number == SIGUSR2)
+		queue.content[queue.end] = queue.content[queue.end] | (bit >> index);
+	index++;
+	if(index == 8)
+	{
+		queue.end = (queue.end + 1)%4096;
+		index = 0;
+	}
+
 }
 void seg_scream(int a){
 	(void) a;
@@ -15,15 +35,41 @@ void seg_scream(int a){
 int main(void)
 {
 	struct sigaction handle;
+	// struct sigaction handle2;
+	queue.content = malloc(4096);
+
+	printf("PID: %d\n" , getpid());
+
+
 	handle.sa_handler = &scream;
+	sigemptyset(&handle.sa_mask);
+	sigaddset(&handle.sa_mask, SIGUSR1);
+	sigaddset(&handle.sa_mask, SIGUSR2);
+	// handle2.sa_handler = &scream;
+	// sigemptyset(&handle2.sa_mask);
+	// sigaddset(&handle2.sa_mask, SIGUSR1);
+	// sigaddset(&handle2.sa_mask, SIGUSR2);
 	//sigaction(SIGINT, &handle, NULL);
 	sigaction(SIGUSR1, &handle, NULL);
-
+	sigaction(SIGUSR2, &handle, NULL);
+	// sigaction(SIGUSR1, &handle2, NULL);
+	// sigaction(SIGUSR2, &handle2, NULL);
 
 	while(1)
 	{
-		printf("PID: %d\n" , getpid());
-		sleep(5);
+		usleep(10);
+		if(queue.start != queue.end)
+		{
+			if(queue.start > queue.end)
+			{
+				write(1, queue.content , 4096 - queue.start);
+				queue.start = write(1, queue.content , queue.end);
+			}
+			else
+				queue.start += write(1, queue.content , queue.end - queue.start);
+		}
+		else
+			pause();
 	}
 	return(0);
 }
